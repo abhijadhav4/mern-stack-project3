5798598b32011b265549eeea6383da28a055e3d1
Abhijit <abhiuttamraojadhav@gmail.com>
2026-07-21 21:25:07 +0530

    Add GitHub Actions OIDC provider and trust policy

--- terraform/github-actions-oidc.tf
+++ terraform/github-actions-oidc.tf
@@ -1,3 +1,9 @@
+resource "aws_iam_openid_connect_provider" "github_actions" {
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = ["6938fd4d98bab03faadb97b98ad1e7d5f5ad0d2e"]
+}
+
 resource "aws_iam_role" "github_actions" {
   name = "${var.project_name}-${var.environment}-github-actions"
 
@@ -8,7 +14,7 @@ resource "aws_iam_role" "github_actions" {
         Sid    = "GitHubActionsOIDC"
         Effect = "Allow"
         Principal = {
-          Federated = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/token.actions.githubusercontent.com"
+          Federated = aws_iam_openid_connect_provider.github_actions.arn
         }
         Action = "sts:AssumeRoleWithWebIdentity"
         Condition = {
@@ -18,7 +24,7 @@ resource "aws_iam_role" "github_actions" {
           StringLike = {
             "token.actions.githubusercontent.com:sub" = [
               "repo:abhijadhav4/mern-stack-project3:ref:refs/heads/main",
-              "repo:abhijadhav4/mern-stack-project3:pull_request"
+              "repo:abhijadhav4/mern-stack-project3:ref:refs/heads/master"
             ]
           }
         }
@@ -58,7 +64,8 @@ resource "aws_iam_role_policy" "github_actions_ecr_eks" {
       {
         Effect = "Allow"
         Action = [
-          "eks:DescribeCluster"
+          "eks:DescribeCluster",
+          "eks:AccessKubernetesApi"
         ]
         Resource = [
           "arn:aws:eks:${var.aws_region}:${data.aws_caller_identity.current.account_id}:cluster/${var.cluster_name}"
